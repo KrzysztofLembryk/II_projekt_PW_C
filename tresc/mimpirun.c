a4f8d0a80c746901331132de4d00e3c34aab2b98
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-01-05 12:47:00 +0100

    removed printfs from mimpirun

--- tresc/mimpirun.c
+++ tresc/mimpirun.c
@@ -4,44 +4,28 @@
 
 #include "mimpi_common.h"
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char *argv[])
+{
     // Minimal nbr of args is 3: programme name, nbr of copies, path
-    if(argc < 3)
+    if (argc < 3)
         return -1;
 
     int nbr_of_copies_to_run = atoi(argv[1]);
     const char *path = argv[2];
-    int other_args = atoi(argv[3]);
 
-    printf("name: %s, copies: %d, path: %s, other: %d\n", argv[0], nbr_of_copies_to_run, path, other_args);
+    // We need null terminated array, so size needs to be + 1
+    int rest_size = argc - 2 + 1;
+    char **rest_of_args = calloc(rest_size, sizeof(char *));
 
-    char **rest_of_args;
-    
-        int n = argc - 2 + 1; // null terminated array we need
-        printf("rest_args ssize: %d\n", n);
-        rest_of_args = calloc(n, sizeof(char*));
+    memcpy(rest_of_args, argv + 2, rest_size * sizeof(*rest_of_args));
 
-        memcpy(rest_of_args, argv + 2, n * sizeof(*rest_of_args));
-        //for(int i = 0; i < n; i++)
-        //    rest_of_args[i] = argv[i + 3];
-
-        printf("many args!!!, restArgs[0]: %s, rest[1]: %s\n\n", 
-            rest_of_args[0], rest_of_args[1]);
-        
-    
-    
-
-    for(int i = 0; i < nbr_of_copies_to_run; i++)
+    for (int i = 0; i < nbr_of_copies_to_run; i++)
     {
         pid_t pid = fork();
         ASSERT_SYS_OK(pid);
 
-        if(!pid)
-        {
-            ASSERT_SYS_OK(execvp(*rest_of_args, rest_of_args));   
-        }
-
+        if (!pid)
+            ASSERT_SYS_OK(execvp(*rest_of_args, rest_of_args));
     }
 
     if (nbr_of_copies_to_run != 0)
@@ -50,5 +34,5 @@ int main(int argc, char* argv[]) {
     free(rest_of_args);
 
     return 0;
-    //TODO
+    // TODO
 }
