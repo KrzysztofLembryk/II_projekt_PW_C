5e42b72300684c1be30efa78c9e3e50efba8c813
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-01-05 17:19:38 +0100

    removed obsolete commented code in mimpirun

--- tresc/mimpirun.c
+++ tresc/mimpirun.c
@@ -6,35 +6,24 @@
 
 int main(int argc, char *argv[])
 {
-    
-    
-
     // Minimal nbr of args is 3: programme name, nbr of copies, path
     if (argc < 3)
         return -1;
 
-    int nbr_of_copies_to_run = atoi(argv[1]);
-
     // We make env var to store process indexes, and env var to store how many
     // processes we run. 
-    
     char *NBR_PROC = "NBR_PROC";
     putenv(NBR_PROC);
     setenv(NBR_PROC, argv[1], 1);
 
     char *PROC_RANK = "PROC_RANK";
     putenv(PROC_RANK);
-    //const char *path = argv[2];
-
-    // We need null terminated array, so size needs to be + 1
-    //int rest_size = argc - 2 + 1;
-    //char **rest_of_args = calloc(rest_size, sizeof(char *));
-    //memcpy(rest_of_args, argv + 2, rest_size * sizeof(*rest_of_args));
     
     // We want all argv arguments starting from argv[2] where name of programme
     // to exec is stored.
     char **rest_of_args = &argv[2];
     char idx_str[12];
+    int nbr_of_copies_to_run = atoi(argv[1]);
 
     for (int i = 0; i < nbr_of_copies_to_run; i++)
     {
@@ -49,15 +38,12 @@ int main(int argc, char *argv[])
             sprintf(idx_str, "%d", i);
             setenv(PROC_RANK, idx_str, 1);
             ASSERT_SYS_OK(execvp(*rest_of_args, rest_of_args));
-        }
-            
+        }    
     }
 
     // We wait for every child.
     for (int i = 0; i < nbr_of_copies_to_run; i++)
         ASSERT_SYS_OK(wait(NULL));
-
-    //free(rest_of_args);
 
     return 0;
     // TODO
